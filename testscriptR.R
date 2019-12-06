6d08683bc191431b8cc0fcca4fb1202a9fc73652
Riin Aljas <aljasriin@gmail.com>
2019-12-06 16:20:43 -0500

    cleaned the code, wrote explanations

--- testscriptR.R
+++ testscriptR.R
@@ -6,55 +6,32 @@ library(fpc)
 library(philentropy)
 library(data.table)
 
-df <- USArrests
-df <- na.omit(df)
-df <- scale(df)
-?scale
+#to be sure that the census data inquiry works 
+#census_api_key('156fda6326a38745b31480cc7848c55e7f4fcf41', overwrite = FALSE, install = TRUE)
 
-distance <- get_dist(tarr)
-fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
+#get data for population and income, so that we would have at least two variables 
+countyincome <- get_acs(geography = "county", variables = c(medincome="B19013_001"))
+population <- get_acs(geography = "county", variables = c(pop= "B00001_001E")) 
 
-census_api_key('156fda6326a38745b31480cc7848c55e7f4fcf41', overwrite = FALSE, install = TRUE)
+population <- population %>% rename(pop = estimate) %>% select(-variable)
 
+countyincome <- left_join(countyincome1, population)
 
-countyincome1 <- get_acs(geography = "county", variables = c(medincome="B19013_001"))
-countyincome2 <- get_acs(geography = "county", variables = c(pop= "B00001_001E")) 
-
-countyincome2 <- countyincome2 %>% rename(pop = estimate) %>% select(-variable)
-
-countyincome <- left_join(countyincome1, countyincome2)
-
+#select only the variables we're interested in 
 countyincome <- countyincome %>%
   select(-variable, -moe)
 
+#continue with test variable
+
 t <- countyincome %>% remove_rownames() %>% column_to_rownames(var="NAME") %>% select(-GEOID)
 
+#be sure there's no NA-s
 t <- na.omit(t)
 
-
-k2 <- kmeans(t, centers = 25, nstart = 6)
-
-fviz_cluster(k2, data = t)
-
-cluster.stats(d, )
-
-distance(t, method = "euclidean")
-
+#calculate the euclidean distance 
 euc <- distance(t, method = "euclidean")
 
-euc <- as.data.frame(euc)
-
-
-#ou can use data.table to do a binary search:
-  
-dt = data.table(euc) # you'll see why val is needed in a sec
-setkey(dt, "euc")
-dt[J(v16), roll = "nearest"]
-dt[J(v16), .I, roll = "nearest", by = .EACHI]
-
-
-rm(x)
-
+#create a function to sort all the counties by which one is the closest to x
 
 close <- function(x, value, tol=NULL){
   if(!is.null(tol)){
@@ -63,6 +40,9 @@ close <- function(x, value, tol=NULL){
     x[order(abs(x-10))]
   }
 }
+
+
+# have rownames as names so we know which places we're talking about 
 euc2 <- euc[1] %>% rownames_to_column()
 y <- as.data.frame(close(euc2$v1, value=12603.618, tol=NULL)) 
 y <- y %>% 
